17a4db84552b6e9cec7216dd7eadfe538341c5e3
elmagnifico <elmagnificogg@gmail.com>
2021-02-27 10:40:41 +0800

    add new script

--- maya/RVOImportFromMaya.py
+++ maya/RVOImportFromMaya.py
@@ -1,27 +1,36 @@
-f = open(r"F:\Github\RVO2-3D-update\examples\179.txt","r")
+f = open(r"F:\Github\RVO2-3D-update\examples\407.txt", "r")
 import json
+
 data = json.load(f)
 f.close()
 
-spheres_num = len(data[0])/3
-print "spheres num:"+str(spheres_num)
+spheres_num = len(data[0]) / 3
+print "spheres num:" + str(spheres_num)
 
 sum_frames = len(data)
 print sum_frames
 
 base_name = "dmd"
 
-#for i in range(spheres_num):
+# for i in range(spheres_num):
 #    cmds.polySphere(name=base_name+'#')
 
 cur_time = 1
 
+spheres = []
+for i in range(spheres_num):
+    name = base_name + str((i) + 1)
+    spheres.append(name)
 
-
-for cur in range(sum_frames):
-    cmds.currentTime(cur +1)
-    for i in range(0,len(data[cur*10]),3):
-        pos = [data[cur*10][i],data[cur*10][i+1],data[cur*10][i+2]]
-        name = base_name + str((i/3)+1)
+data_interval = 1  # 10
+time_interval = 1
+cmds.cutKey(spheres, time=(cur_time, len(data) / data_interval * time_interval + 1000), clear=True)
+for line in range(0, len(data), data_interval):
+    cmds.currentTime(cur_time)
+    data_index = line
+    for i in range(0, len(data[data_index]), 3):
+        pos = [data[data_index][i], data[data_index][i + 1], data[data_index][i + 2]]
+        name = base_name + str((i / 3) + 1)
         cmds.move(pos[0] + 2, pos[1], pos[2], name, ws=True)
         cmds.setKeyframe(name, attribute='translate')
+    cur_time += time_interval
