a58ba0ba5dd4947121f06835463fa635677d4d3d
elmagnifico <elmagnificogg@gmail.com>
2021-03-11 18:30:57 +0800

    update script

--- DeleteCode/delete.cpp
+++ DeleteCode/delete.cpp
@@ -1,21 +1,21 @@
-	/* Set the preferred velocity to be a vector of unit magnitude (speed) in the direction of the goal. */
-	for (size_t index = 0; index < sim.getNumAgents(); ++index) {
-		Vector3 goalVector = Vector3(end_position[index].x, end_position[index].y, end_position[index].z) - sim.getAgentPosition(index);
+/* Set the preferred velocity to be a vector of unit magnitude (speed) in the direction of the goal. */
+for (size_t index = 0; index < sim.getNumAgents(); ++index) {
+	Vector3 goalVector = Vector3(end_position[index].x, end_position[index].y, end_position[index].z) - sim.getAgentPosition(index);
 
-		if (filter[index] <= (rel_time / frame_time * 1000.0 + start_frame+0.001))
-		{
-			if (absSq(goalVector) > 1.0f) {
-				goalVector = normalize(goalVector);
-			}
-			sim.setAgentRadius(index, safe_dis);
-			sim.setAgentPrefVelocity(index, goalVector);
+	if (filter[index] <= (rel_time / frame_time * 1000.0 + start_frame+0.001))
+	{
+		if (absSq(goalVector) > 1.0f) {
+			goalVector = normalize(goalVector);
 		}
-		else
-		{
-			//debug_print << "wait:" << index << endl;
-			cur_position[index] = start_position[index];
-			sim.setAgentPrefVelocity(index, Vector3(0, 0, 0));
-			sim.setAgentRadius(index, 0.0);
-			sim.setAgentPosition(index, Vector3(start_position[index].x, start_position[index].y, start_position[index].z));
-		}
+		sim.setAgentRadius(index, safe_dis);
+		sim.setAgentPrefVelocity(index, goalVector);
 	}
+	else
+	{
+		//debug_print << "wait:" << index << endl;
+		cur_position[index] = start_position[index];
+		sim.setAgentPrefVelocity(index, Vector3(0, 0, 0));
+		sim.setAgentRadius(index, 0.0);
+		sim.setAgentPosition(index, Vector3(start_position[index].x, start_position[index].y, start_position[index].z));
+	}
+}
