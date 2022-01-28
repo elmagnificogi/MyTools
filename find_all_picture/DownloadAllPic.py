353c5921d1f3d8bf80d813e912333632e6caa978
elmagnifico <elmagnificogg@gmail.com>
2022-01-28 16:29:01 +0800

    add new py for replace and upload all img in my blog

--- find_all_picture/DownloadAllPic.py
+++ find_all_picture/DownloadAllPic.py
@@ -6,21 +6,39 @@ import requests
 
 # first get all file
 dir = r"E:\Github\elmagnificogi.github.io\_posts"
-target_str = "![*](*)"
-#origin_str = "Raspberrypi-head-bg.jpg"
-#replace_str = "Raspberrypi-head-bg.png"
+dir = r"E:\elmagnificogi.github.io\_posts"
+
+# token
+token = "not use my token"
+
+# sapic address
+sapic_url = "img.elmagnifico.tech:9514"
+
 
 def download(url):
-    print ("download:"+url)
+    print("download:" + url)
     header = {}
     r = requests.get(url, headers=header, stream=True)
-    print(r.status_code) # 返回状态码
+    print(r.status_code)  # 返回状态码
     file_name = url.split("/")[-1]
-    print("file:"+file_name)
+    print("file:" + file_name)
     if r.status_code == 200:
-        open('.\img\\'+file_name, 'wb').write(r.content) # 将内容写入图片
+        open('.\img\\' + file_name, 'wb').write(r.content)  # 将内容写入图片
         print("done")
     del r
+
+
+def uploadByUrl(url):
+    print("upload:" + url)
+    headers = {"Authorization": "LinkToken " + token}
+    requests.post(
+        "http://" + sapic_url + "/api/upload",
+        data=dict(
+            picbed=url
+        ),
+        headers=headers,
+    ).json()
+
 
 all_img_url = []
 
@@ -31,15 +49,11 @@ for file in os.listdir(dir):
     f = open(file_path, encoding='utf-8')
     content = f.readlines()
     f.close()
-    # print(content)
-    # replace it
-    #f = open(file_path, 'w', encoding='utf-8')
-    #new_file = ""
     for line in content:
         new_line = line
         # print(line)
-        if re.search(r'!\[(.*)\]\((.*)\)',line) != None:
-            # # check some un SM.MS pic
+        if re.search(r'!\[(.*)\]\((.*)\)', line) != None:
+            # # check some not in SM.MS pic
             # if "loli" in line:
             #     continue
             # else:
@@ -47,25 +61,20 @@ for file in os.listdir(dir):
             # split imgs in one line
             line = line.strip()
             img_url = line.split("![")
-            #print(line)
+            # print(line)
             for url in img_url:
                 if url != "":
-                    #print("!["+url)
-                    #print(url)
-                    #print(url.split("("))
+                    # print("!["+url)
+                    # print(url)
+                    # print(url.split("("))
                     real_url = (url.split("("))[1]
-                    #print(real_url)
+                    # print(real_url)
                     real_url = real_url.split(")")[0]
                     all_img_url.append(real_url)
                     print(real_url)
-                    download(real_url)
+                    ##download(real_url)
+                    uploadByUrl(real_url)
             continue
-        elif re.search(r'<img src',line) != None:
+        elif re.search(r'<img src', line) != None:
             # check old img link
             print(line)
-            #print(line.replace(origin_str, replace_str))
-            #new_line = line.replace(origin_str, replace_str)
-
-        #new_file += new_line
-    #f.write(new_file)
-    #f.close()
