b732d262ead4630ef148fd69c8b44ca81ac5a97b
elmagnifico <elmagnificogg@gmail.com>
2022-03-24 22:42:49 +0800

    add new script

---
+++ git_commit_info/rename.bat
@@ -0,0 +1,12 @@
+@echo off
+:: echo %1%
+for /F %%i in ('git describe --tags --always --abbrev^=8 HEAD') do (set tagid=%%i)
+:: echo %tagid%
+set "path=%1%
+set "path=%path:/=\%"
+:: echo %path%
+@echo on
+del %path%\dmd_uav(%tagid%).bin /q
+ren %path%\dmd_uav.bin dmd_uav(%tagid%).bin
+
+
