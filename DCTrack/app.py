cbd586ee238a2349ed3a0b116e408ca82d068fdb
elmagnifico <elmagnificogg@gmail.com>
2022-05-14 20:55:36 +0800

    add new script

---
+++ DCTrack/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, jsonify
+import requests
+import threading
+import time
+
+app = Flask(__name__)
+d2api = "https://diablo2.io/dclone_api.php"
+server_socket = ""
+dcTrackInfo = ""
+
+user_count = 0
+last_user_count = 0
+
+def getDCInfo():
+    global dcTrackInfo
+    call = requests.get(d2api).json()
+    dcTrackInfo = call
+
+
+def DCTrackLoop():
+    global last_user_count
+    global user_count
+    while True:
+        try:
+            getDCInfo()
+        except:
+            pass
+        time.sleep(5)
+        last_user_count = user_count
+        user_count = 0
+
+
+@app.route('/', methods=['GET'])
+def dctrackinfo():
+    global user_count
+    user_count += 1
+    return jsonify(dcTrackInfo)
+
+
+@app.route('/s', methods=['GET'])
+def statistics():
+    global last_user_count
+    return "user count:" + str(last_user_count)
+
+
+if __name__ == '__main__':
+    # start tracker
+    getDCInfo()
+    dctrack = threading.Thread(target=DCTrackLoop)
+    dctrack.start()
+    app.run(host='0.0.0.0', port=8000)
--- DCTrack/main.py
+++ DCTrack/main.py
@@ -1,3 +1,4 @@
+import datetime
 import socket
 import requests
 import threading
@@ -9,6 +10,8 @@ d2api = "https://diablo2.io/dclone_api.php"
 server_socket = ""
 dcTrackInfo = ""
 
+now = time.time()
+user_count = 0
 
 def getDCInfo():
     global dcTrackInfo
@@ -17,6 +20,8 @@ def getDCInfo():
     dcTrackInfo = call
 
 def handle_client(client_socket):
+    global user_count
+    global now
     """
     处理客户端请求
     """
@@ -31,6 +36,12 @@ def handle_client(client_socket):
     # 向客户端返回响应数据
     client_socket.send(bytes(response, "utf-8"))
 
+    cur = time.time()
+    user_count+=1
+    if cur - now > 5:
+        now = cur
+        print("User Num:"+str(user_count))
+        user_count = 0
     # 关闭客户端连接
     client_socket.close()
  
@@ -38,7 +49,8 @@ def socketInit():
     global server_socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(("", 8000))
-    server_socket.listen(128)
+    server_socket.listen(65530)
+
 
 def DCTrackLoop():
     while True:
@@ -46,7 +58,8 @@ def DCTrackLoop():
             getDCInfo()
         except:
             pass
-        time.sleep(5000)
+        time.sleep(5)
+    print("DCTrackLoop End")
 
 if __name__ == "__main__":
     getDCInfo()
@@ -58,9 +71,13 @@ if __name__ == "__main__":
     while True:
         try:
             client_socket, client_address = server_socket.accept()
-            print("[%s, %s]用户连接上了" % client_address)
+            #print("[%s, %s]用户连接上了" % client_address)
             handle_client_process = Process(target=handle_client, args=(client_socket,))
             handle_client_process.start()
             client_socket.close()
         except:
             socketInit()
+        #print("Restart a new socket")
+
+    print("DCTrack End")
+    server_socket.close()
