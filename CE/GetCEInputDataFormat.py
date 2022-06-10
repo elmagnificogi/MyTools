7b9dcf53e10659ff08505fb0f71e6180f71d793b
elmagnifico <elmagnificogg@gmail.com>
2022-06-11 01:21:34 +0800

    new mode for find same segment

--- CE/GetCEInputDataFormat.py
+++ CE/GetCEInputDataFormat.py
@@ -6,38 +6,42 @@ data2 = f2.readlines()[0].split(" ")
 f1.close()
 f2.close()
 
-len = min(len(data1),len(data2))
+len = min(len(data1), len(data2))
 
-print (data1)
-print (data2)
-
-ce_data_format = ""
-for i in range(len):
-    ce_data_format+=data1[i]
-    if i < len-2:
-        ce_data_format+=','
-print "ce data1:"
-print (ce_data_format)
+print(data1)
+print(data2)
 
 ce_data_format = ""
 for i in range(len):
-    ce_data_format+=data2[i]
-    if i < len-2:
-        ce_data_format+=','
-print "ce data2:"
-print (ce_data_format)
+    ce_data_format += data1[i]
+    if i < len - 1:
+        ce_data_format += ','
+print("ce data1:")
+print(ce_data_format)
+
+ce_data_format = ""
+for i in range(len):
+    ce_data_format += data2[i]
+    if i < len - 1:
+        ce_data_format += ','
+print("ce data2:")
+print(ce_data_format)
 
 search_pattern = ""
 search_data = ""
+same_data = ""
 for i in range(len):
     if data1[i] == data2[i]:
-        search_data+="\\x"+data1[i]
-        search_pattern+= "x"
+        search_data += "\\x" + data1[i]
+        search_pattern += "x"
+        same_data += data1[i]+','
     else:
-        search_data+="\\x"+data1[i]
-        search_pattern+= "?"
+        search_data += "\\x" + data1[i]
+        search_pattern += "?"
+        print(same_data)
+        same_data = ""
 
-print "search data:"
-print search_data
-print "search pattern"
-print search_pattern
+print("search data:")
+print(search_data)
+print("search pattern")
+print(search_pattern)
