06457a0821ae01e3e8cfb6cd09bb8c3323b0cfd8
elmagnifico <elmagnificogg@gmail.com>
2023-02-27 23:09:16 +0800

    update script

---
+++ find_all_picture/ReplaceVideoLink.py
@@ -0,0 +1,41 @@
+import os
+import re
+#import requests
+
+# use for replace old url
+
+# first get all file
+#dir = r"E:\Github\elmagnificogi.github.io\_posts"
+dir = r"E:\elmagnificogi.github.io\_posts"
+
+# replace url
+replace_url = "https://img.elmagnifico.tech/static/upload/elmagnifico/"
+
+all_img_url = []
+
+for file in os.listdir(dir):
+    # show file name
+    print(file)
+    if not ".md" in file:
+        continue
+    file_path = dir + "\\" + file
+    f = open(file_path, 'r', encoding='utf-8')
+    content = f.readlines()
+    f.close()
+
+    lines = ""
+    for line in content:
+        new_line = ""
+        # print(line)
+        if re.search(r'<iframe src="//(.*)', line) != None:
+            print(line)
+            new_line = line.replace("src=\"//","src=\"https://")
+            print(new_line)
+        else:
+            new_line = line
+
+        lines += new_line
+
+    f = open(file_path, 'w', encoding='utf-8')
+    f.write(lines)
+    f.close()
