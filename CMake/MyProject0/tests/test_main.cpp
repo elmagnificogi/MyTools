f8f3479b4acab3957a28a85efe9b034020af92c4
zhourongxin <elmagnificogg@gmail.com>
2024-12-19 11:38:27 +0800

    update script

--- CMake/MyProject0/tests/test_main.cpp
+++
@@ -1,6 +0,0 @@
-//#include <gtest/gtest.h>
-
-// Test cases for MyLib
-TEST(MyLibTest, BasicTest) {
-    EXPECT_EQ(1, 1);
-}
--- CMake/MyProject1/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-// Declarations of module functions
-
-#endif // MAIN_H
